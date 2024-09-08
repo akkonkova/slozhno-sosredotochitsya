55ed90ca1d1008fec85a202fc96ff7e25e3ca38f
Anna Konkova <yndx.annkonkova@yandex.ru>
2024-09-08 17:23:51 +0300

    finished html and style for first content-section

--- stylelint.config.js
+++ stylelint.config.js
@@ -22,7 +22,7 @@ module.exports = {
     'declaration-block-single-line-max-declarations': 1,
     'no-unknown-animations': true,
     'selector-type-case': 'lower',
-    'rule-empty-line-before': 'always',
+    // 'rule-empty-line-before': 'always',
     'at-rule-empty-line-before': 'always',
     'selector-class-pattern': null,
     'font-family-name-quotes': 'always-where-recommended',
